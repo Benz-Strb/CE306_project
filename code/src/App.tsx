4d28a1ab49aecf7d6ef0bb88fd1edb7cca2f630c
Benz-Strb <nd.natthawutbenz@gmail.com>
2025-10-12 22:08:22 +0700

    .

---
+++ code/src/App.tsx
@@ -0,0 +1,13 @@
+// Tailwind Check
+
+import './App.css'
+
+function App() {
+  return (
+    <h1 className='txet-3xl font-bold underline text-blue-600'>
+      Hello World
+    </h1>
+  )
+}
+
+export default App
